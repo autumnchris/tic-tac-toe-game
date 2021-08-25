b597c2b109896f82a7b6f11f6a7205fb4fc4de67
autumnchris <autumncbullard@gmail.com>
2021-08-25 11:42:22 -0400

    Add markup for the header and footer

--- src/modules/app.js
+++ src/modules/app.js
@@ -1,7 +1,16 @@
+import starWarsLogo from '../images/star-wars-logo.png';
+
 const App = (() => {
 
   function renderApp() {
-    document.getElementById('app').innerHTML = '';
+    document.getElementById('app').innerHTML = `
+    <header>
+      <h1>
+        <img src="${starWarsLogo}" class="star-wars-logo" alt="Star Wars" />
+      </h1>
+      <h2>Tic-Tac-Toe</h2>
+    </header>
+    <footer>Created by <a href="https://autumnbullard-portfolio.herokuapp.com" target="_blank">Autumn Bullard</a> &copy; ${new Date().getFullYear()}</footer>`;
   }
 
   return {
