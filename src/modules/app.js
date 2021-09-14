463f438102e3ec7fb8a55ecf61966c28b33e04d5
autumnchris <autumncbullard@gmail.com>
2021-09-14 12:40:35 -0400

    Create 2-player game

--- src/modules/app.js
+++ src/modules/app.js
@@ -1,4 +1,5 @@
 import starWarsLogo from '../images/star-wars-logo.png';
+import { Game } from './game';
 
 const App = (() => {
 
@@ -14,6 +15,14 @@ const App = (() => {
       <div class="game-container"></div>
     </main>
     <footer>Created by <a href="https://autumnbullard-portfolio.herokuapp.com" target="_blank">Autumn Bullard</a> &copy; ${new Date().getFullYear()}</footer>`;
+
+    Game.startNewGame('2-player');
+
+    document.addEventListener('click', event => {
+      const element = event.target;
+      element.matches('.square') ? Game.playTurn(event) : null;
+      element.matches('.restart-button') ? Game.startNewGame('2-player') : null;
+    });
   }
 
   return {
--- src/modules/game.js
+++ src/modules/game.js
@@ -1,5 +1,25 @@
+import { NewGameModal } from './new-game-modal';
+
 const Game = (() => {
-  let board = new Array(9).fill('');
+  const winningCombinations = [
+    [ 0, 1, 2 ],
+    [ 3, 4, 5 ],
+    [ 6, 7, 8 ],
+    [ 0, 3, 6 ],
+    [ 1, 4, 7 ],
+    [ 2, 5, 8 ],
+    [ 0, 4, 8 ],
+    [ 2, 4, 6 ]
+  ];
+  let board;
+  let currentPlayer;
+
+  const Player = (playerType, playAs) => {
+    return {
+      playerType,
+      playAs
+    };
+  };
 
   function renderGameBoard(firstPlayer) {
     document.querySelector('.game-container').innerHTML = `<div class="board-container">
@@ -15,6 +35,69 @@ const Game = (() => {
       </div>
     </div>`;
   }
+
+  function startNewGame(players, difficulty = null, playAs = null) {
+    let playerOne;
+    let playerTwo;
+    board = new Array(9).fill('');
+
+    if (players === '1-player') {
+      console.log('One player game...');
+    }
+    else {
+      playerOne = Player('human', 'Rebellion');
+      playerTwo = Player('human', 'Empire');
+    }
+    currentPlayer = playerOne.playAs;
+    renderGameBoard(playerOne.playAs);
+    console.log(playerOne);
+    console.log(playerTwo);
+    NewGameModal.closeNewGameModal();
+  }
+
+  function playTurn(event) {
+
+    if (!board[event.target.id]) {
+      board[event.target.id] = currentPlayer.toLowerCase();
+      document.getElementById(event.target.id).classList.add(`fa-${currentPlayer === 'Rebellion' ? 'rebel' : 'empire'}`, currentPlayer.toLowerCase(), 'selected');
+
+      if (checkForWinner(currentPlayer)) {
+        endGame(currentPlayer);
+      }
+      else if (checkForDraw()) {
+        endGame('draw');
+      }
+      else {
+        currentPlayer === 'Rebellion' ? currentPlayer = 'Empire' : currentPlayer = 'Rebellion';
+        document.querySelector('.current-turn-message').innerHTML = `It's the ${currentPlayer}'s turn.`;
+      }
+    }
+  }
+
+  function checkForWinner(player) {
+    return winningCombinations.some(combination => {
+      return combination.every(val => {
+        return board[val] === player.toLowerCase();
+      });
+    });
+  }
+
+  function checkForDraw() {
+    return board.every(square => {
+      return square;
+    });
+  }
+
+  function endGame(winner) {
+    let endGameMessage;
+    winner === 'draw' ? endGameMessage = 'It\'s a draw!' : endGameMessage = `The ${winner} won!`;
+    NewGameModal.openNewGameModal(endGameMessage, winner);
+  }
+
+  return {
+    startNewGame,
+    playTurn
+  };
 })();
 
 export { Game };
